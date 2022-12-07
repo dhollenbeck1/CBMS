2719a4502b9e062ad099551c0dfd4d1b3049af41
Derek Hollenbeck <30532879+dhollenbeck1@users.noreply.github.com>
2022-12-06 22:34:58 -0800

    update model

--- simulate_chenRinconMoraModel.m
+++ simulate_chenRinconMoraModel.m
@@ -1,13 +1,20 @@
 clc
 clear
 
+% -- NOTES
+% state of charge varies between 0 < V_SOC < 1
+T = 2000;
+dt = 1e-3;
+n = round(T/dt);
+t = linspace(0,T,n)';
+R_load = 0.4; %[t,0.4*ones(n,1)];
+
 % -- Battery Lifetime
-params.capacity = 25; % AH
-params.f1 = 0.9; % cycle number correction factor
-params.f2 = 0.9; % temperature correction factor
-% 0 < V_SOC < 1
-params.C_capacity = 3600*params.capacity*params.f1*params.f2;
-params.R_selfDischarge = 1e12;
+p.capacity = 25; % AH
+p.f1 = 0.9; % cycle number correction factor
+p.f2 = 0.9; % temperature correction factor
+p.C_capacity = 3600*p.capacity*p.f1*p.f2;
+p.R_selfDischarge = 1e12;
 
 % -- Voltage-Current Characteristics 
 fn.V_oc     = @(SOC,p)  p(1)*exp(p(2)*SOC) + p(3)*SOC.^3 + p(4)*SOC.^2 + p(5)*SOC + p(6);
@@ -18,13 +25,20 @@ fn.R_L      = @(SOC,p)  p(1)*exp(p(2)*SOC) + p(3);
 fn.C_L      = @(SOC,p)  p(1)*exp(p(2)*SOC) + p(3);
 
 % -- Voltage-Current Characteristics
-params.V_oc         = [-1.031,-35,0.3201,-0.1178,0.2156,3.685];
-params.R_series     = [0.1562,-24.37,0.07446];
-params.R_S          = [0.3208,-29.14,0.04669];
-params.C_S          = [-752.9,-13.51,703.6];
-params.R_L          = [6.603,-155.2,0.04984];
-params.C_L          = [-6056,-27.12,4475];
+p.V_oc         = [-1.031,-35,0.3201,-0.1178,0.2156,3.685];
+p.R_series     = [0.1562,-24.37,0.07446];
+p.R_S          = [0.3208,-29.14,0.04669];
+p.C_S          = [-752.9,-13.51,703.6];
+p.R_L          = [6.603,-155.2,0.04984];
+p.C_L          = [-6056,-27.12,4475];
 
+% -- Variables
+v.SOC         = 1;
+v.V_oc        = fn.V_oc(v.SOC,p.V_oc);
+v.R_series    = fn.R_series(v.SOC,p.R_series);
+v.R_S         = fn.R_S(v.SOC,p.R_S);
+v.C_S         = fn.C_S(v.SOC,p.C_S);
+v.R_L         = fn.R_L(v.SOC,p.R_L);
+v.C_L         = fn.C_L(v.SOC,p.C_L);
 
-
-
+sim("chenRinconMoraModel.slx")
